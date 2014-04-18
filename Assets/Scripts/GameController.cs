c02d0754b3b2729cdecfe12ee72a264585d5ab4d
mizzouman <krjt28@mail.missouri.edu>
2014-04-18 16:23:51 -0500

    added the asteroid field

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -22,7 +22,59 @@ public class GameController : MonoBehaviour
 		{
 			for (int i = 0; i < hazardCount; i++)
 			{
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Vector3 spawnPosition;
+
+
+				switch(random)
+				{
+					case 0:
+					{
+						spawnPosition = new Vector3 (Random.Range ((-spawnValues.x)-5, -spawnValues.x), Random.Range((-spawnValues.y)-5, -spawnValues.y), Random.Range((-spawnValues.z)-5, -spawnValues.z));
+						random++;
+						break;
+					}
+					case 1:
+					{
+						spawnPosition = new Vector3 (Random.Range (spawnValues.x, (spawnValues.x)+5), Random.Range(spawnValues.y, (spawnValues.y)+5), Random.Range(spawnValues.z, (spawnValues.z)+5));
+						random++;
+						break;
+					}
+					case 2:
+					{
+						spawnPosition = new Vector3 (Random.Range (spawnValues.x, (spawnValues.x)+5), Random.Range((-spawnValues.y)-5, -spawnValues.y), Random.Range((-spawnValues.z)-5, -spawnValues.z));
+						random++;
+						break;
+					}
+					case 3:
+					{
+						random++;
+						spawnPosition = new Vector3 (Random.Range ((-spawnValues.x)-5, -spawnValues.x), Random.Range(spawnValues.y, (spawnValues.y)+5), Random.Range(spawnValues.z, (spawnValues.z)+5));
+						break;
+					}
+					case 4:
+					{
+						spawnPosition = new Vector3 (Random.Range ((-spawnValues.x)-5, -spawnValues.x), Random.Range((-spawnValues.y)-5, -spawnValues.y), Random.Range(spawnValues.z, (spawnValues.z)+5));
+						random++;
+						break;
+					}
+					case 5:
+					{
+						spawnPosition = new Vector3 (Random.Range ((-spawnValues.x)-5, -spawnValues.x), Random.Range(spawnValues.y, (spawnValues.y)+5), Random.Range((-spawnValues.z)-5, -spawnValues.z));
+						random++;
+						break;
+					}
+					case 6:
+					{
+						spawnPosition = new Vector3 (Random.Range (spawnValues.x, (spawnValues.x)+5), Random.Range(spawnValues.y, (spawnValues.y)+5), Random.Range((-spawnValues.z)-5, -spawnValues.z));
+						random++;
+						break;
+					}
+					default:
+					{
+					spawnPosition = new Vector3 (Random.Range (spawnValues.x, (spawnValues.x)+5), Random.Range((-spawnValues.y)-5, -spawnValues.y), Random.Range(spawnValues.z, (spawnValues.z)+5));
+						random = 0;
+						break;
+					}
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
