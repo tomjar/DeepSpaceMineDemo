6928199f1eeed41ab6df8cfa57cc4894d0be0c96
tomjar <tomreese86@gmail.com>
2014-04-17 22:12:34 -0500

    importing assets, project settings, and project data
    importing assets, project settings, and project data

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		//you hit the asteroid!
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		//asteroid hit player
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.GameOver ();
+			//Application.Quit();
+			//Reese 4/16/2014, simply reload the game if they are hit TODO: do something else other than reload the game :-D
+			Application.LoadLevel(0);
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello I was called!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Reese 4-16-2014 this will simply exit the game after the escape key is pressed.
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		
+	}
+}
